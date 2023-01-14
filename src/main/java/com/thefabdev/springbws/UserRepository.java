bacd86a6c839d49d17d87f9b85100ff577567b0c
fakorede <abiolafakorede@gmail.com>
2023-01-14 12:12:07 +0100

    user repository

---
+++ src/main/java/com/thefabdev/springbws/UserRepository.java
@@ -0,0 +1,11 @@
+package com.thefabdev.springbws;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thefabdev.springbws.io.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+	UserEntity findUserByEmail(String email);
+}
--- src/main/java/com/thefabdev/springbws/service/UserService.java
+++ src/main/java/com/thefabdev/springbws/service/UserService.java
@@ -1,17 +1,35 @@
 package com.thefabdev.springbws.service;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.thefabdev.springbws.UserRepository;
+import com.thefabdev.springbws.io.entity.UserEntity;
 import com.thefabdev.springbws.service.contract.UserServiceContract;
 import com.thefabdev.springbws.shared.dto.UserDto;
 
 @Service
 public class UserService implements UserServiceContract {
+	
+	@Autowired
+	UserRepository userRepository;
 
 	@Override
 	public UserDto createUser(UserDto user) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);
+		
+		userEntity.setEncryptedPassword("test");
+		userEntity.setUserId("testUserId");
+		
+		UserEntity savedUser = userRepository.save(userEntity);
+		
+		UserDto data = new UserDto();
+		BeanUtils.copyProperties(savedUser, data);
+		
+		return data;
 	}
 
 }
